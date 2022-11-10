237ce7d6f3e713ec904bb628103974711a62e69b
Julia Melnikova <fox5rafy@gmail.com>
2022-11-10 15:18:54 +0300

    Connected Onegin; added func get_strings_tree; begin to wtite func parse_data

--- source/akinator.c
+++ source/akinator.c
@@ -10,8 +10,14 @@ int play(tree_t* tree)
 {
     CHECK(tree != NULL, ERR_TREE_NULL_PTR);
 
-    FILE* file = open_file("in_data.txt");
-    CHECK(file != NULL, ERR_TREE_BAD_FILE);
+    FILE* text = open_file("in_data.txt");
+    CHECK(text != NULL, ERR_AKTR_BAD_FILE);
+
+    struct string_t* strings_tree = NULL;
+    strings_tree = get_strings_tree(text);
+    CHECK(strings_tree != NULL, ERR_TREE_NULL_PTR);
+
+    fclose(text);
 
     int mode = get_mode();
     CHECK(mode != MODE_ERROR, ERR_AKTR_BAD_MODE);
@@ -36,6 +42,8 @@ int play(tree_t* tree)
 
     return TREE_SUCCESS;
 }
+
+//=========================================================================
 
 int get_mode()
 {
@@ -63,4 +71,29 @@ int get_mode()
     }
 
     return MODE_ERROR;
+}
+
+//=========================================================================
+// from onegin
+void get_strings_tree(FILE* text, struct string_t* strings)
+{
+    long count = count_symbols(text);
+    HANDLE_ERROR(count, ERR_TXT_BAD_PTR, "ERROR: pointer outside file.\n");
+
+    char* buffer = (char*) calloc(count, sizeof(char));
+    int ret = fill_buffer(text, buffer, sizeof(char), count);
+    HANDLE_ERROR(ret, ERR_TXT_BAD_READ, "ERROR: file read error.\n");
+
+    long number_strings = count_strings(buffer, count);
+    strings = get_strings(buffer, count, number_strings);
+
+}
+
+//=========================================================================
+
+int parse_data(struct string_t* strings_tree, node_t* node)
+{
+
+
+    return AKTR_SUCCESS;
 }
